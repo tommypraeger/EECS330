3ff8420aef9faba9db2ab12534dfdf95c3d4d358
Tommy Praeger <tommypraeger@dhcp-10-105-154-113.wireless.northwestern.private>
2018-02-16 14:13:32 -0600

    hi

--- home_page.js
+++ home_page.js
@@ -13,7 +13,6 @@ function create() {
     window.location.href = "daily.html";
 =======
     window.location.href = "http://htmlpreview.github.io/?https://github.com/tommypraeger/EECS330/blob/master/daily.html";
->>>>>>> 9d482b14db365e3dcf3ebe5c37be294d5b5cd784
   }
 }
 
@@ -36,4 +35,3 @@ document.getElementById("confirm").addEventListener("keyup", function(event) {
         document.getElementById("create").click();
   }
 });
->>>>>>> 9d482b14db365e3dcf3ebe5c37be294d5b5cd784
