9395456ba507577e5c73d1ea3b37a5a9adc43297
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2018-02-15 23:25:18 -0600

    Update home_page.js

--- home_page.js
+++ home_page.js
@@ -1,1 +1,23 @@
+function confirm_password(){
+    document.getElementById("confirm").style.display="block";
+    document.getElementById("create").style.display="block";
+    document.getElementById("signup").style.display="none";
+    document.getElementById("login").style.display="none";
+};
+
+function create() {
+  if (document.getElementById("password").value != document.getElementById("confirm").value) {
+    alert("Passwords don't match!")
+  } else {
+    window.location.href = "daily.html";
+  }
+}
+
+function checkempty() {
+  if (document.getElementById("password").value == '' && document.getElementById("confirm").value == '') {
+    alert("Enter username and password")
+  } else {
+    window.location.href = "daily.html";
+  }
+}
 
