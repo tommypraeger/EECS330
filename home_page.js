bc8860cb41f4e8bc8e71b4f25aafa26a4967332a
danielb311 <34021040+danielb311@users.noreply.github.com>
2018-02-16 13:38:08 -0600

    Update home_page.js

--- home_page.js
+++ home_page.js
@@ -21,3 +21,9 @@ function checkempty() {
   }
 }
 
+document.getElementById("confirm").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("create").click();
+  }
+});
