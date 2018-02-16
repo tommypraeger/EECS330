c9d18291117fc9fef400aeaddbb4122c1307a412
Tommy Praeger <tommypraeger@dhcp-10-105-154-113.wireless.northwestern.private>
2018-02-16 13:47:12 -0600

    hello

---
+++ daily.js
@@ -0,0 +1,18 @@
+
+
+// When the user clicks the button, open the modal
+function openmodal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closemodal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == document.getElementById("myModal")) {
+        document.getElementById("myModal").style.display = "none";
+    }
+}
